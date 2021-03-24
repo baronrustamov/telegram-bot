5775fe5ec1819f9ec1e8a957c22bf5e80175e890
baronrustamov <baronrustamov@gmail.com>
2021-03-24 10:39:20 +0300

    cmt

--- run11.py
+++ run11.py
@@ -20,18 +20,32 @@ import sys
 import dialogflow
 import time
 import datetime as dtm
-from telegram import ParseMode, MessageEntity, ChatAction
 from telegram.error import BadRequest, Unauthorized
 #from telegram.ext import CommandHandler, Updater, MessageHandler, Filters, CallbackContext
 from telegram.utils.helpers import escape_markdown
+
 from google.protobuf.json_format import MessageToJson, MessageToDict, Parse
 from google.cloud import vision_v1
 from google.cloud.vision_v1 import types
+
+'''
+import const
+from components import inlinequeries, taghints
+from const import (ENCLOSING_REPLACEMENT_CHARACTER, GITHUB_PATTERN, OFFTOPIC_CHAT_ID, OFFTOPIC_RULES,
+                   OFFTOPIC_USERNAME, ONTOPIC_RULES, ONTOPIC_USERNAME, ONTOPIC_RULES_MESSAGE_LINK,
+                   OFFTOPIC_RULES_MESSAGE_LINK, ONTOPIC_RULES_MESSAGE_ID,
+                   OFFTOPIC_RULES_MESSAGE_ID)
+'''
+
+#from util import get_reply_id, reply_or_edit, get_text_not_in_entities, github_issues, rate_limit, rate_limit_tracker
+
 
 from telegram.ext import Updater, CommandHandler, Filters, \
     MessageHandler, InlineQueryHandler
 import telegram
 from telegram import InlineQueryResultArticle, InputTextMessageContent
+
+#from telegram import ParseMode, MessageEntity, ChatAction, Update, Bot, InlineQueryResultArticle, InputTextMessageContent
 
 from wit import Wit
 from wit.wit import WitError
@@ -41,16 +55,10 @@ from lang import NOT_UNDERSTOOD
 import img_rec
 from img_rec import recog
 
-import pymysql.cursors
 
-'''
-#from util import get_reply_id, reply_or_edit, get_text_not_in_entities, github_issues, rate_limit, rate_limit_tracker
-#from telegram import ParseMode, MessageEntity, ChatAction, Update, Bot, InlineQueryResultArticle, InputTextMessageContent
-from telegram import ParseMode, MessageEntity, ChatAction, Update, Bot
-from telegram.error import BadRequest, Unauthorized
-from telegram.ext import CommandHandler, Updater, MessageHandler, Filters, CallbackContext
-from telegram.utils.helpers import escape_markdown
-'''
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'vidkey.json'
+
+
 '''
 import const
 from components import inlinequeries, taghints
@@ -72,19 +80,18 @@ def notify_admins(message):
             logging.warning('Admin chat_id %s unreachable', admin_id)
 
 botinfo = '''
-Добро пожаловать в пекарню
-<b>Между нами булочками!</b>
-<a href="http://54.189.52.114">Заходите на наш сайт</a>
+Hi there! 
+I'm ur helper bot
+Enjoy!
 '''
 
 def start(bot, update):
     chat_id = update.message.chat_id
     bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
     reply = botinfo
-    bot.send_message(chat_id=chat_id, text=reply, parse_mode=ParseMode.HTML, disable_web_page_preview=False)
+    bot.send_message(chat_id=chat_id, text=reply)
     reply1 = dialogflow_event_request('TELEGRAM_WELCOME', chat_id)
     bot.send_message(chat_id=chat_id, text=reply1)
-
 
 '''    
 def tghelp(bot, update):
@@ -94,68 +101,10 @@ def tghelp(bot, update):
     bot.send_message(chat_id=chat_id, text=reply)
 '''
 
-def subscripslist(bot, update):
-    chat_id = update.message.chat_id
-    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
-    connection = pymysql.connect(host='54.189.52.114',
-                                 user='ubuntu',
-                                 password='zyrzak-kizfa4-nAcdog',
-                                 database='mainbulka',
-                                 cursorclass=pymysql.cursors.DictCursor)
 
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `contactFirstName`, `contactLastName`, `phone`, `addressLine1` FROM mainbulka.customers"
-        cursor.execute(sql)
-        res = cursor.fetchall()
-        res1 = json.JSONEncoder().encode(res)
-        res2 = json.loads(res1)
-        count = cursor.rowcount
-        reply = ''
-        i = 0
-        for i in range(0, count, 1):
-            reply = reply + '<b>' + res2[i]["contactFirstName"] \
-                    +' ' + res2[i]["contactLastName"] +'</b>' \
-                    + ':\n' + res2[i]["phone"] +'\n'\
-                    + res2[i]["addressLine1"] + '\n' + '\n'
-        cursor.close()
-    connection.close()
-    chatstr = "-485348087 76978130"
-    if str(chat_id) in chatstr:
-        bot.send_message(chat_id=chat_id, text=reply, parse_mode=ParseMode.HTML)
-    else: bot.send_message(chat_id=chat_id, text='U R NOT ADMIN', parse_mode=ParseMode.HTML)
-    #update.message.reply_text("Okay.", quote=True)
+result_storage_path = 'tmp'
 
-def productslist(bot, update):
-    chat_id = update.message.chat_id
-    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
-    connection = pymysql.connect(host='54.189.52.114',
-                                 user='ubuntu',
-                                 password='zyrzak-kizfa4-nAcdog',
-                                 database='wordpress',
-                                 cursorclass=pymysql.cursors.DictCursor)
 
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `post_title`, `post_excerpt` FROM `wp_posts` WHERE `post_type` IN ('product')"
-        cursor.execute(sql)
-        res = cursor.fetchall()
-        res1 = json.JSONEncoder().encode(res)
-        res2 = json.loads(res1)
-        count = cursor.rowcount
-        reply = ''
-        i = 0
-        for i in range(0,count,1):
-            reply = reply +'<b>'+ res2[i]["post_title"] +'</b>' + ':\n' + res2[i]["post_excerpt"] + '\n'+'\n'
-        #print(res3)
-        # dbinfo = pymysql.Connection.close(connection)
-        # out = result[0]
-        # print(out)
-        print(pymysql.get_client_info())
-        cursor.close()
-    connection.close()
-    bot.send_message(chat_id=chat_id, text=reply, parse_mode=ParseMode.HTML)
-    #update.message.reply_text("Okay.", quote=True)
 
 def sandwich(bot, update):
     chat_id = update.message.chat_id
@@ -170,9 +119,9 @@ def tghelp(bot, update):
     #reply = dialogflow_event_request('TELEGRAM_WELCOME', chat_id)
     #bot.send_message(chat_id=chat_id, text=reply)
     update.message.reply_text(
-        'Наш сайт [здесь](http://54.189.52.114/). '
-        'Присоединяйся!',
-        quote=True, disable_web_page_preview=False, parse_mode=ParseMode.MARKDOWN)
+        'The on-topic group is [here](https://telegram.me/pythontelegrambotgroup). '
+        'Come join us!',
+        quote=True, disable_web_page_preview=False)#, parse_mode=ParseMode.MARKDOWN)
 
 def text(bot, update):
     chat_id = update.message.chat_id
@@ -222,8 +171,13 @@ def img(bot, update):
     dataimg = json.loads(res)
     jdump = json.dumps(res, indent=4)
     #print(dataimg["webEntities"][0]["description"])
+
+    print(dataimg)
+    print(res)
+
     print(res)
     print(dataimg)
+
     print(jdump)
 
     out = dataimg["webEntities"][0]["description"] + '\n' + '\n'
@@ -333,7 +287,6 @@ def ogg_to_mp3(ogg_path, mp3_path):
                              "-y", mp3_path], stderr=subprocess.PIPE)
     logging.debug(proc.stderr.read().decode())
 
-
 logging.info('Program started')
 
 # Init dialogflow
@@ -358,9 +311,10 @@ notify_admins('Bot started')
 
 # set commands
 UPDATER.bot.set_my_commands([
-    ('/start', 'Запускает бота заново'),
-    ('/help', 'Помощь в использовании бота.'),
-    ('/products', 'Список товаров в продаже'),
+    ('docs', 'Send the link to the docs. Use in PM.'),
+    ('wiki', 'Send the link to the wiki. Use in PM.'),
+    ('hints', 'List available tag hints. Use in PM.'),
+    ('help', 'Send the link to this bots README. Use in PM.'),
 ])
 
 # Add telegram handlers
@@ -369,12 +323,6 @@ DISPATCHER.add_handler(START_HANDLER)
 
 TGHELP_HANDLER = CommandHandler('help', tghelp)
 DISPATCHER.add_handler(TGHELP_HANDLER)
-
-PRODUCTS_LIST = CommandHandler('products', productslist)
-DISPATCHER.add_handler(PRODUCTS_LIST)
-
-SUBSCRIPTIONS_LIST = CommandHandler('subs', subscripslist)
-DISPATCHER.add_handler(SUBSCRIPTIONS_LIST)
 
 sandwich_handler = MessageHandler(Filters.regex(r'(?i)[\s\S]*?((sudo )?make me a sandwich)[\s\S]*?'),
                                       sandwich)
@@ -386,10 +334,8 @@ DISPATCHER.add_handler(img_handler)
 
 TEXT_HANDLER = MessageHandler(Filters.text, text)
 DISPATCHER.add_handler(TEXT_HANDLER)
-
 INLINE_HANDLER = InlineQueryHandler(inline)
 DISPATCHER.add_handler(INLINE_HANDLER)
-
 if WIT_TOKEN:
     VOICE_HANDLER = MessageHandler(Filters.voice, voice)
     DISPATCHER.add_handler(VOICE_HANDLER)
